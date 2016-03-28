2013a705ca892baea79d49a5bf208cb9f0c75041
Kim Blomqvist <kblomqvist@iki.fi>
2016-03-28 21:51:28 +0300

    Adding ACK and STOP

--- modules/i2c/master.vhd
+++ modules/i2c/master.vhd
@@ -25,20 +25,22 @@ end i2c;
 -------------------------------------------------------
 
 architecture behv of i2c is
-  type state_t is (idle, sread, swrite, sstop);
+  type state_t is (idle, read, write, stop, test_ack);
   signal state : state_t := idle;
   signal scl_i : std_logic;
 
 begin
-
-  process(clk, reset_n) -- Handles SDA and state
+  process(clk, reset_n)
     variable rwbit : std_logic;
     variable i : integer range 0 to 7;
+
   begin
     if reset_n='0' then
       sda <= 'Z';
+      scl <= 'Z';
       state <= idle;
-    elsif rising_edge(clk) then -- CLK RISING EDGE
+
+    elsif rising_edge(clk) then
       case state is
       when idle =>
         sda <= 'Z';
@@ -46,22 +48,40 @@ begin
           i := 7;
           sda <= '0';
           rwbit := shift_register(0);
-          state <= swrite;
+          state <= write;
         end if;
-      when swrite =>
+      when write =>
         if scl='0' and shift_register(i)='0' then
           sda <= '0';
         elsif scl='0' and shift_register(i)='1' then
           sda <= 'Z';
         else -- SCL is high. It's time to index next bit.
-          if i > 0 then
-          i := i - 1;
+          if i=0 then
+            state <= test_ack;
+          else
+            i := i - 1;
           end if;
         end if;
-      when sread =>
-        if scl='1' then
-          i := i - 1; -- Next bit
+      when test_ack =>
+        i := 7;
+        sda <= 'Z';
+        if scl='Z' then
+          if sda='0' and rwbit='1' then
+            state <= read;
+          elsif sda='0' and rwbit='0' then
+            state <= write;
+          else
+            state <= stop;
+          end if;
         end if;
+      when stop =>
+        if scl='0' then
+          sda <= '0';
+        elsif scl='Z' and sda='0' then
+          sda <= 'Z';
+          state <= idle;
+        end if;
+
 
 --        state <= ack when (i=0) else state;
 --        state <= sread when rwbit='1' else state;
@@ -70,32 +90,18 @@ begin
       when others =>
         state <= state;
       end case;
-    elsif falling_edge(clk) then -- CLK FALLING EDGE
-      case state is
-      when sread =>
---        shift_register(i) <= sda when scl='1';
---        state <= ack when i=0;
-        state <= state;
-      when others =>
-        state <= state;
-      end case;
-    end if;
-  end process;
 
-  process(clk, reset_n) -- Handles SCL according to state
-  begin
-    if reset_n='0' then
-      scl_i <= '1';
     elsif falling_edge(clk) then
+      scl_i <= not scl_i;
       case state is
       when idle =>
         scl_i <= '1';
-
       when others =>
-        scl_i <= not scl_i;
+        state <= state;
       end case;
     end if;
   end process;
+
   scl <= '0' when scl_i='0' else 'Z';
 
 end behv;
--- testbench/i2c_master_tb/i2c_master_tb.vhd
+++ testbench/i2c_master_tb/i2c_master_tb.vhd
@@ -1,67 +1,77 @@
 ----------------------------------------------------------
--- Design  : Simple testbench for an 8-bit VHDL counter
--- Author  : Javier D. Garcia-Lasheras
+-- Design  : I2c MAster
+-- Author  : Kim Blomqvist
 ----------------------------------------------------------
 
 library ieee;
-use ieee.std_logic_1164.all;			 
+use ieee.std_logic_1164.all;
 
-entity i2c_master_tb is			-- entity declaration
+entity i2c_master_tb is     -- entity declaration
 end i2c_master_tb;
 
 -----------------------------------------------------------------------
 
 architecture testbench of i2c_master_tb is
 
-    component i2c
-    port(   
-		  scl : inout std_logic;  -- Bus clock pin
-		  sda : inout std_logic;  -- Bus data pin
-		  clk : in std_logic;     -- Bus speed is clk/2
-		  start_n : in std_logic; -- Issues start from hi->lo and
-		                          -- stop from lo->hi transition
-		  reset_n : in std_logic;
-		  shift_register : inout std_logic_vector(7 downto 0)
-    );
-    end component;
+  component i2c
+  port(
+      scl : inout std_logic;  -- Bus clock pin
+      sda : inout std_logic;  -- Bus data pin
+      clk : in std_logic;     -- Bus speed is clk/2
+      start_n : in std_logic; -- Issues start from hi->lo and
+                  -- stop from lo->hi transition
+      reset_n : in std_logic;
+      shift_register : inout std_logic_vector(7 downto 0)
+  );
+  end component;
 
-    signal t_scl: std_logic;
-    signal t_sda: std_logic;
-    signal t_clk:     std_logic;
-    signal t_start:     std_logic;
-    signal t_reset:     std_logic;
-    signal t_sr:         std_logic_vector(7 downto 0);
+  signal t_scl: std_logic;
+  signal t_sda: std_logic;
+  signal t_clk:     std_logic;
+  signal t_start:     std_logic;
+  signal t_reset:     std_logic;
+  signal t_sr:         std_logic_vector(7 downto 0);
 
-    signal t_clear:     std_logic;
-    signal t_count:     std_logic;
+  signal t_clear:     std_logic;
+  signal t_count:     std_logic;
 
 begin
-    
-    i2c_u: i2c port map (t_scl, t_sda, t_clk, t_start, t_reset, t_sr);
-	
-    process				 
-    begin -- clock (200 kHz)
-		t_clk <= '0';
-		wait for 2 ns;
-		t_clk <= '1';
-		wait for 2 ns;
-    end process;
-	
-    process
-    begin				
-    	t_sr <= "10100101";
-    	t_start <= '1';
-    	t_reset <= '0';
-		wait for 3 ns;	
-    	t_reset <= '1';
-		wait for 3 ns;	
-    	t_start <= '0';
 
-		report "Testbench of Adder completed successfully!" 
-		severity note; 
-	wait;
-		
-    end process;
+  i2c_u: i2c port map (t_scl, t_sda, t_clk, t_start, t_reset, t_sr);
+
+  process
+  begin -- clock (200 kHz)
+    t_clk <= '0';
+    wait for 2 ns;
+    t_clk <= '1';
+    wait for 2 ns;
+  end process;
+
+  process
+  begin
+    t_sr <= "11010010";
+    t_start <= '1';
+    t_reset <= '0';
+    t_sda <= 'Z';
+    wait for 3 ns;
+    t_reset <= '1';
+    wait for 3 ns;
+    t_start <= '0';
+    wait for 10 ns;
+    t_start <= '0';
+    wait for 58 ns;
+    t_sda <= 'Z'; -- ACK
+    wait for 8 ns;
+    t_sda <= 'Z';
+    t_sr <= "11110101";
+    wait for 10 ns;
+    t_start <= '1';
+
+    report "Testbench of Adder completed successfully!"
+    severity note;
+  wait;
+
+  end process;
 
 end testbench;
 
